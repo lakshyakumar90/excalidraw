29a6ddb66396aa6260b17de50d863e1a64d24ce2
lakshyakumar90 <lakshyakumar5023@gmail.com>
2026-09-17 16:47:33 +0530

    created common package

---
+++ packages/common/src/constants.ts
@@ -0,0 +1,5 @@
+export const MIN_ZOOM = 0.1;
+export const MAX_ZOOM = 30;
+export const DEFAULT_ZOOM = 1;
+export const DEFAULT_SCROLL_X = 0;
+export const DEFAULT_SCROLL_Y = 0;
---
+++ packages/common/src/types.ts
@@ -0,0 +1,15 @@
+export interface Point{
+  x: number,
+  y: number,
+}
+
+export interface Viewport {
+  scrollX: number,
+  scrollY: number,
+  zoom: number,
+}
+
+export interface Size {
+  width: number,
+  height: number,
+}
